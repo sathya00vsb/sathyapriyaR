674940b7d825ef03f3a88663616b994935e16479
sathya00vsb <31504220+sathya00vsb@users.noreply.github.com>
2017-09-04 22:38:32 +0530

    Create Y.c

---
+++ Y.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+Void  main()
+{ 
+int  y;
+Printf("Enter the value of y");
+if( y℅4==0)
+Printf("It is a leap year");
+ else
+ Printf("It is not a leap year");
+ }
