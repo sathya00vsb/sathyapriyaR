3252f7fee21c308d290c91d13eaf29700138b6b4
sathya00vsb <31504220+sathya00vsb@users.noreply.github.com>
2017-09-07 00:04:53 +0530

    Create L.c

---
+++ L.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+Void  main()
+{ 
+Int  y;
+Printf("Enter the value of y");
+Scanf(%d,&y);
+If(y%4==0)
+Printf("it  is a leap year");
+Else 
+Printf( "it is not a leap year");
+} 
